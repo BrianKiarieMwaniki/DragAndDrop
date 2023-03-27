6cd3a7c4b9b94becb4d2fc6825d887a5b5baa9be
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-03-27 10:48:57 +0300

    Add project files.

---
+++ DragAndDrop/Models/Job.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DragAndDrop.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public JobStatus Status { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
---
+++ DragAndDrop/Models/JobStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace DragAndDrop
+{
+    public enum JobStatus
+    {
+        Started = 1,
+        OnGoing = 2,
+        OnHold = 3,
+        Completed = 4,
+        Cancelled = 5,
+    }
+}
